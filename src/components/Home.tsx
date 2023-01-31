99655cd3b85f2dcd7f5c25b63d8aafa17560a1c0
Gabriel <gabrigomez15@gmail.com>
2023-01-31 15:46:35 -0300

    feat: add getArtist method

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,17 @@ import './Home.css';
 interface Token {
   access_token: string;
 }
+interface Artist {
+  id: string,
+}
 
 export const Home = () => {
-  const [publicToken, setPublicToken] = useState<Token>({access_token: ''});  
+  const [publicToken, setPublicToken] = useState<Token>({access_token: ''}); 
+  const [artist, setArtist] = useState<Artist>({id: ''});
+  const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
 
-  const getAccessToken = async() => {
-    const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-    const clientId = process.env.REACT_APP_CLIENT_ID;
-      
+  const getAccessToken = async() => {      
     const result = await fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
         headers: {
@@ -25,6 +28,20 @@ export const Home = () => {
     setPublicToken(data);
   }
 
+  const getArtist = async() => {
+    const url = 'https://api.spotify.com/v1/search?q=Tool&type=artist&limit=1'    
+    const result = await fetch(url, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + publicToken.access_token}      
+    });
+
+    const data = await result.json();
+    console.log(data.artists.items[0])
+    if(data.artists.items[0]) {
+      setArtist(artist.id = data.artists.items[0].id)
+    }
+  }
+
   return (
     <div className='home-container'>
       <h1>
@@ -33,6 +50,9 @@ export const Home = () => {
       <div>
         <button onClick={getAccessToken}>
           Connect
+        </button>
+        <button onClick={getArtist}>
+          Get Artist
         </button>
         {publicToken ? (
           <div>
