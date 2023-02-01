bfbe1a4828eefd45e157122f2b5580dd3e06aff3
Gabriel <gabrigomez15@gmail.com>
2023-02-01 14:45:28 -0300

    feat: start birthday-festival component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
+import { BirthdayFestival } from './components/BirthdayFestival';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/birthday-festival' element={<BirthdayFestival />} />
         </Routes>
       </main>
     </div>
---
+++ src/components/BirthdayFestival.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const BirthdayFestival = () => {
+  return (
+    <div>
+      <h1>My Birthday Festival</h1>
+    </div>
+  )
+}
