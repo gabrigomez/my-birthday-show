05c238001b7737ed387d1e0726214064cddbbacc
Gabriel <gabrigomez15@gmail.com>
2023-02-01 16:19:11 -0300

    fix: remove redux for now and fix the home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,14 +3,13 @@ import './Home.css';
 
 export const Home = () => {
   const [userToken] = useState(localStorage.getItem("token"));
-  const clientId = process.env.REACT_APP_CLIENT_ID;
-  
+  const clientId = process.env.REACT_APP_CLIENT_ID;  
   const authEndpoint = 'https://accounts.spotify.com/en/authorize';
   const redirectUri = 'http://localhost:3000';
 
   useEffect(() => {
-    const hash = window.location.hash 
-    let token = localStorage.getItem("token")
+    const hash = window.location.hash; 
+    let token = localStorage.getItem("token");
 
     if (!token && hash) {
       const tokenHash = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))?.split("=")[1];
@@ -18,10 +17,13 @@ export const Home = () => {
       
       if(tokenHash) {
         localStorage.setItem("token", tokenHash);
+        window.location.replace(redirectUri);
       }
     };
 
   }, []);
+
+  console.log(userToken)
 
   return (
     <div className='home-container'>
@@ -29,7 +31,7 @@ export const Home = () => {
         HOME
       </h1>
       <div>
-        {userToken && (
+        {!userToken && (
           <a href={`${authEndpoint}?response_type=token&redirect_uri=${redirectUri}&client_id=${clientId}&scope=user-top-read&state=96r77`}>
             Login to Spotify
           </a>
--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-import { store } from './store';
-import { Provider } from 'react-redux';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <React.StrictMode>    
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
