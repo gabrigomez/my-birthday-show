befaec2d0c915623d359ee81159ebf73fdfa0792
Gabriel <gabrigomez15@gmail.com>
2023-02-02 11:35:56 -0300

    fix: disable the strict mode to prevent duplicate calls to spotify api

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,56 +6,42 @@ export const Home = () => {
   const clientId = process.env.REACT_APP_CLIENT_ID;  
   const clientSecret = process.env.REACT_APP_CLIENT_SECRET
   const authEndpoint = 'https://accounts.spotify.com/en/authorize';
-  const redirectUri = 'http://localhost:3000';
+  const redirectUri = 'http://localhost:3000/';
 
-  const authorizationCode = window.btoa(`${clientId}:${clientSecret}`)
-
-  // useEffect(() => {
-  //   const hash = window.location.hash; 
-  //   let token = localStorage.getItem("token");
-
-  //   if (!token && hash) {
-  //     const tokenHash = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))?.split("=")[1];
-  //     window.location.hash = "";
-      
-  //     if(tokenHash) {
-  //       localStorage.setItem("token", tokenHash);
-  //       window.location.replace(redirectUri);
-  //     }
-  //   };
-
-  // }, []);
-
-  const getToken = async(code:string) => {
-    const headers = {
-      Authorization: `Basic ${authorizationCode}`,
-      'Content-Type' : 'application/x-www-form-urlencoded',
-    };
-
-    const data = new URLSearchParams();
-    data.append('code', code);
-    data.append('redirect_uri', redirectUri)
-    data.append('grant_type', 'authorization_code');
-
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      headers,
-      method: 'POST',
-      body: data,
-    });
-
-    const token = await response.json();    
-    setUserToken(token);
-  }
-
+  const authorizationCode = window.btoa(`${clientId}:${clientSecret}`);
+  
   useEffect(() => {
     const href = window.location.href
     const code = href.substring(1)?.split("=")[1];
 
     if(code) {
+      const getToken = async(code:string) => {
+        const headers = {
+          Authorization: `Basic ${authorizationCode}`,
+          'Content-Type' : 'application/x-www-form-urlencoded',
+        };
+    
+        const data = new URLSearchParams();
+        data.append('code', code);
+        data.append('redirect_uri', redirectUri)
+        data.append('grant_type', 'authorization_code');
+    
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+          headers,
+          method: 'POST',
+          body: data,
+        });
+    
+        if(response.ok) {
+          const token = await response.json();    
+          setUserToken(token);
+        };    
+      };
+
       getToken(code);     
     };
     
-  }, []);
+  }, [authorizationCode]);
 
   console.log(userToken)
 
@@ -66,9 +52,6 @@ export const Home = () => {
       </h1>
       <div>
         {userToken && (
-          // <a href={`${authEndpoint}?response_type=token&redirect_uri=${redirectUri}&client_id=${clientId}&scope=user-top-read&state=96r77`}>
-          //   Login to Spotify
-          // </a>
           <a href={`${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-currently-playing%20user-top-read`}>
             Login to Spotify
           </a>
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>    
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+root.render(      
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>  
 );
 
 // If you want to start measuring performance in your app, pass a function
