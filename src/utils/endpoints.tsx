a0425bd6ac903f2110cdbe2fcab92883efad75d5
Gabriel <gabrigomez15@gmail.com>
2023-02-02 16:50:53 -0300

    feat: add limit of 16 musics to the list

--- src/utils/endpoints.tsx
+++ src/utils/endpoints.tsx
@@ -1,8 +1,8 @@
 const clientId = process.env.REACT_APP_CLIENT_ID;  
 
-export const TOP_TRACKS_SHORT = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term';
-export const TOP_TRACKS_MEDIUM = 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term';
-export const TOP_TRACKS_LONG = 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term';
+export const TOP_TRACKS_SHORT = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=16';
+export const TOP_TRACKS_MEDIUM = 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=16';
+export const TOP_TRACKS_LONG = 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=16';
 
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/en/authorize';
 export const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
