5fe7ffa0282ba9b1117614adb0bcbd47115af761
Gabriel <gabrigomez15@gmail.com>
2023-02-02 16:28:00 -0300

    feat: add styles to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
 import { login } from '../features/userSlice';
 import { RootState } from '../store';
 import { AUTH_FULL_ENDPOINT, REDIRECT_URI, TOKEN_ENDPOINT } from '../utils/endpoints';
+
+import { SlSocialSpotify } from 'react-icons/sl';
+import { BiCake } from 'react-icons/bi';
+import { FaMusic } from 'react-icons/fa';
 
 export const Home = () => {
   const clientId = process.env.REACT_APP_CLIENT_ID;  
@@ -50,18 +55,34 @@ export const Home = () => {
   }, [authorizationCode, token, haveAccess, navigate, dispacth]);
 
   return (
-    <div className='flex flex-col items-center'>
-      <div>
-        <h1 className='text-5xl'>
+    <div className='flex flex-col h-full items-center justify-center bg-gradient-to-t from-white to-gray-300'>
+      <div className='flex flex-col items-center'>
+        <div className='flex mb-1'>
+          <BiCake className='text-5xl' />
+          <FaMusic className='text-2xl text-green-400'/>
+        </div>
+        <h1 className='text-5xl mb-2'>
           My Birthday Festival
         </h1>
-        <h3>E se sua festa de aniversário fosse um festival?</h3>
+        <h3 className=''>
+          E se sua festa de aniversário fosse um festival?
+        </h3>
       </div>
       <div>
         {!haveAccess && (
-          <a href={AUTH_FULL_ENDPOINT}>
-            Login to Spotify
-          </a>
+          <div className='flex flex-col items-center'>
+            <a 
+              className='flex mt-4 p-2 items-center border rounded-full bg-green-400 hover:bg-green-500 duration-200 group' 
+              href={AUTH_FULL_ENDPOINT}>
+                <SlSocialSpotify className='text-3xl text-white' />
+                <p className='ml-1 group-hover:text-gray-800'>
+                  Login to Spotify
+                </p>
+            </a>
+            <a className='text-xs mt-2 hover:text-blue-700 duration-300' href="/">
+              Termos de uso
+            </a>
+          </div>
         )}      
       </div>
     </div>
