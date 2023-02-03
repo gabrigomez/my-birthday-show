f737a9a1c895129b4b945cc1c18a337c05a90a0e
Gabriel <gabrigomez15@gmail.com>
2023-02-03 16:08:37 -0300

    fix: move redirect_uri to environment variables

--- src/utils/endpoints.tsx
+++ src/utils/endpoints.tsx
@@ -1,4 +1,5 @@
-const clientId = process.env.REACT_APP_CLIENT_ID;  
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const Uri = process.env.REACT_APP_REDIRECT_URI;  
 
 export const TOP_TRACKS_SHORT = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=13';
 export const TOP_TRACKS_MEDIUM = 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=13';
@@ -6,7 +7,7 @@ export const TOP_TRACKS_LONG = 'https://api.spotify.com/v1/me/top/tracks?time_ra
 
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/en/authorize';
 export const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
-export const REDIRECT_URI = 'http://localhost:3000/';
+export const REDIRECT_URI = `${Uri}`;
 
 export const AUTH_FULL_ENDPOINT = `${AUTH_ENDPOINT}?client_id=${clientId}&response_type=code&redirect_uri=${REDIRECT_URI}&
 scope=user-read-currently-playing%20user-top-read`;
